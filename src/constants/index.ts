e1bc0a14486ec3f4f755ed8b71a01f632a57124a
Sergey Sudakov <s3gaddtv@gmail.com>
2023-01-22 15:26:50 +0300

    refactor: use webSocket stream

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,6 +17,7 @@ export enum PORTS {
 
 export const SCREENSHOT_SIZE = 200;
 
-export const RESULT_MESSAGE  = 'Result';
-export const SUCCESS_MESSAGE = 'completed successfully';
-export const FAILURE_MESSAGE = 'command ended with error';
+export const RESULT_MESSAGE    = 'Result';
+export const SUCCESS_MESSAGE   = 'completed successfully';
+export const FAILURE_MESSAGE   = 'command ended with error';
+export const APP_ERROR_MESSAGE = 'Application error';
--- src/wss/index.ts
+++ src/wss/index.ts
@@ -1,8 +1,8 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 
 import { commandHandler } from './commands';
 
-import { FAILURE_MESSAGE, RESULT_MESSAGE } from '../constants';
+import { FAILURE_MESSAGE, RESULT_MESSAGE, APP_ERROR_MESSAGE } from '../constants';
 
 export const startWebSocketServer = (port: number) => {
     console.log(`Start webSocket server on the ${port} port!`);
@@ -14,7 +14,12 @@ export const startWebSocketServer = (port: number) => {
 
         console.log(`Connected new user with ${ip} ip. Total clients count: ${wss.clients.size}`);
 
-        ws.on('message', async data => {
+        const duplex = createWebSocketStream(ws, {
+            decodeStrings: false,
+            encoding: 'utf8',
+        });
+
+        duplex.on('data', async (data: string) => {
             const [ command, ...params ] = data.toString().split(' ');
             console.log(`Received ${command} command from client with '${params}' params`);
 
@@ -22,16 +27,23 @@ export const startWebSocketServer = (port: number) => {
                 const result = await commandHandler(command, params.join(' '));
                 console.log(`${RESULT_MESSAGE}: ${result}`);
 
-                ws.send(result);
+                duplex.write(result);
             } catch(error) {
                 console.log(`${command} ${FAILURE_MESSAGE}: ${error.message}`);
             }
         });
 
+        duplex.on('error', error => console.log(`${APP_ERROR_MESSAGE}: ${error.message}`));
+
         ws.on('close', () => {
-            console.log(`Client ${ip} disconnected. Total clients count: ${wss.clients.size}`)
+            console.log(`Client ${ip} disconnected. Total clients count: ${wss.clients.size}`);
+
+            duplex.destroy();
         });
+
     });
+
+    wss.on('error', error => console.log(`${APP_ERROR_MESSAGE}: ${error.message}`));
 
     process.on('SIGINT', () => {
         console.log('Close webSocket server with all clients connection');
